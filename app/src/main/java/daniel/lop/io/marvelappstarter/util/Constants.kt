0cd23cc84bce89a6307510e1858e5c15e4694515
Joaquim Ventura <nowjoka28@gmail.com>
2023-05-05 20:02:18 -0300

    Adicionado as configuracoes de api e de injecao de dependencias

---
+++ app/src/main/java/daniel/lop/io/marvelappstarter/util/Constants.kt
@@ -0,0 +1,13 @@
+package daniel.lop.io.marvelappstarter.util
+
+object Constants {
+
+    const val BASE_URL = "https://gateway.marvel.com/V1/public/"
+
+    const val PUBLIC_KEY = "584b470a3b72b295ba87f4fb2f3a84d9"
+    const val PRIVATE_KEY = "809e3fc34c83fa63de1161637104ec16138e0630"
+
+    const val APIKEY = "apikey"
+    const val HASH = "hash"
+    const val TS = "ts"
+}
